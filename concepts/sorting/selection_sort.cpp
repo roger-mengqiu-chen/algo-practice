be0513de77e2dd8cd88e4a6b1f18a681c9f24683
Roger Chen <rogermchen.ubc@gmail.com>
2024-03-17 14:48:44 -0600

    selection sort

---
+++ concepts/sorting/selection_sort.cpp
@@ -0,0 +1,32 @@
+#include <vector>
+
+using namespace std;
+
+
+class SelectionSort
+{
+private:
+    void swap(int& a, int& b)
+    {
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+
+public:
+    void solution(vector<int>& lst)
+    {
+        for(int i = 0; i < lst.size() - 1; i++)
+        {
+            int min_idx = i;
+            for (int j = i + 1; j < lst.size(); j++)
+            {
+                if (lst[j] < lst[min_idx])
+                {
+                    min_idx = j;
+                }
+            }
+            swap(lst[i], lst[min_idx]);
+        }
+    }
+};
