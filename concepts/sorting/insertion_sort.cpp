266b4bf9d7ecd446edf27242e0539a0cbe25a977
Roger Chen <rogermchen.ubc@gmail.com>
2024-03-20 11:46:21 -0600

    recursive version of insertion sort

--- concepts/sorting/insertion_sort.cpp
+++ concepts/sorting/insertion_sort.cpp
@@ -5,7 +5,7 @@ using namespace std;
 class InsertionSort 
 {
 public:
-    vector<int> sort(vector<int>& arr) 
+    void sort(vector<int>& arr) 
     {   
         for (int i = 1; i < arr.size(); ++i)
         {
@@ -18,6 +18,22 @@ public:
             }
             arr[j + 1] = key;
         }
-        return arr;
+    }
+
+    void recursive_sort(vector<int>& arr, int n)
+    {
+        if(n <= 1) return;
+
+        recursive_sort(arr, n - 1);
+
+        int last = arr[n-1];
+        int j = n - 2;
+
+        while(j >= 0 && arr[j] > last)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = last;
     }
 };
