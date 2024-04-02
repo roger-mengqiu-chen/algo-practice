3ef5249370d57010d498d787a4cfd4cdc42bd7b8
Roger Chen <rogermchen.ubc@gmail.com>
2024-04-02 10:50:40 -0600

    add binary

--- leetcode/add_binary.cpp
+++ leetcode/add_binary.cpp
@@ -29,10 +29,10 @@ public:
             int x = ac + bc + f;
             switch(x)
             {
-                case 144: res.insert(0, "0"); f = '0'; break;
-                case 145: res.insert(0, "1"); f = '0'; break;
-                case 146: res.insert(0, "0"); f = '1'; break;
-                case 147: res.insert(0, "1"); f = '1'; break;
+                case 144: res.insert(0, "0"); f = '0'; break;  // 0 + 0 + 0
+                case 145: res.insert(0, "1"); f = '0'; break;  // 1 + 0 + 0
+                case 146: res.insert(0, "0"); f = '1'; break;  // 1 + 1 + 0
+                case 147: res.insert(0, "1"); f = '1'; break;  // 1 + 1 + 1
             }
         }
         if(f > '0') res.insert(0, "1");
