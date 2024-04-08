72676f34071d9c5a94282f5e275b864724981032
Roger Chen <roger.chen@datadrill.ca>
2024-04-08 11:09:44 -0600

    remove duplicates

---
+++ src/leetcode/remove_duplicates_from_sorted_list.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode *n;
+        ListNode *h = head;
+        while(h != NULL)
+        {
+            n = h->next;
+            while(n != NULL && n->val == h->val)
+            {
+                n = n->next;
+            }
+            h->next = n;
+            h = n;
+        }
+        return head;
+    }   
+};
+
+int main()
+{
+    ListNode l(2);
+    ListNode l2(1, &l);
+    ListNode l3(1, &l2);
+    // ListNode l4(1, &l3);
+    // ListNode l5(1, &l4);
+
+    for(ListNode *p = &l3; p != 0; p = p->next)
+    {
+        cout << p->val << " ";
+    }
+    cout << endl;
+
+    Solution s;
+    s.deleteDuplicates(&l3);
+
+    for(ListNode *p = &l3; p != 0; p = p->next)
+    {
+        cout << p->val << " ";
+    }
+    cout << endl;
+    return 0;
+}
