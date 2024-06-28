ebfbaeca0793d0c85863ceca0ccb56a2092b695d
Roger Chen <rogermchen.ubc@gmail.com>
2024-06-28 13:46:45 -0600

    weighted quick union

--- src/concepts/search/quick_union.cpp
+++ src/concepts/search/quick_union.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+
+
 class QuickUnion {
 private:
     int* id;
@@ -30,3 +33,56 @@ public:
         delete[] id;
     }
 };
+
+
+class WeightedQuickUnion {
+private:
+    int* id;
+    int* size_id;
+    int size;
+
+    int root(int i) {
+        while(id[i] != i) {
+            id[i] = id[id[i]];
+            i = id[i];
+        }
+        return i;
+    }
+public:
+    WeightedQuickUnion(int n) : size(n) {
+        id = new int[n];
+        size_id = new int[n];
+        for(int i = 0; i < n; i++) {
+            id[i] = i;
+            size_id[i] = 1;
+        }
+    }
+
+    bool connected(int p, int q) {
+        return root(p) == root(q);
+    }
+
+    void weighted_union(int a, int b) {
+        int root_a = root(a);
+        int root_b = root(b);
+
+        if(size_id[root_a] < size_id[root_b]) {
+            // if subset of root_a is smaller, attach subset of root_a to root_b
+            id[root_a] = id[root_b];
+            size_id[root_b] += size_id[root_a];
+        }
+        else {
+            id[root_b] = id[root_a];
+            size_id[root_a] += size_id[root_b];
+        }
+    }
+};
+
+int main() {
+    WeightedQuickUnion u = WeightedQuickUnion(10);
+    u.weighted_union(1, 3);
+    u.weighted_union(3, 4);
+    u.weighted_union(8, 9);
+    std::cout << u.connected(1, 4) << std::endl;
+    std::cout << u.connected(1, 9) << std::endl;
+}
